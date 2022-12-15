fab02a790b2d837c5afee60c5f9952d30c26ba30
jordislpm <jordislpm@gmail.com>
2022-12-15 13:55:56 -0400

    dia 13 resuelto

--- Dia 13/Dia13.js
+++ Dia 13/Dia13.js
@@ -1,3 +1,29 @@
 function getFilesToBackup(lastBackup, changes) {
-    return []
+    const arr = []
+  
+    changes.forEach(([id, timestamp]) => 
+      timestamp > lastBackup && (arr[id] = id)
+    )
+    return arr.filter(a => a)
   }
+
+  let prueba = getFilesToBackup(1556300600, [
+    [ 1, 1546300800 ],
+    [ 2, 1546300800 ],
+    [ 1, 1546300900 ],
+    [ 1, 1546301000 ],
+    [ 3, 1546301100 ]
+  ])
+
+  console.log(prueba)
+
+const lastBackup = 1546300800
+const changes = [
+  [ 3, 1546301100 ],
+  [ 2, 1546300800 ],
+  [ 1, 1546300800 ],
+  [ 1, 1546300900 ],
+  [ 1, 1546301000 ]
+]
+
+console.log(getFilesToBackup(lastBackup, changes));   // => [ 1, 3 ]
