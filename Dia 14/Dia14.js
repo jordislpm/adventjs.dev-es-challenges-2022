54720078b894e6e0d5c8b14476623968101857d9
jordislpm <jordislpm@gmail.com>
2022-12-16 15:39:37 -0400

    seguir con el dia 14

--- Dia 14/Dia14.js
+++ Dia 14/Dia14.js
@@ -1,35 +1,6 @@
 function getOptimalPath(path) {
-    let arr =[];
-    let sum = parseInt(path[0])
-    indice = 0
-    console.log(sum);
-console.log(Math.min(path[1][0], path[1][1]))
-
-path.map(()=>{})
-
-
-console.log(arr);
-console.log(sum)
-    return sum
-  }
-
-  
-  function repeatStringNumTimes(str, num) {
-    let result = " ";
-    let letra = str;
-    for (let i = 0; i < num; i++) {
-        result + str;
-    }
-
-    return result;
-  }
-  
-  ;
-
-  console.log(repeatStringNumTimes("abc", 3));
-
-
-
+  return 0
+}
  // console.log(getOptimalPath([[0], [2, 3]])) // 2
   // 0 -> 2
   
